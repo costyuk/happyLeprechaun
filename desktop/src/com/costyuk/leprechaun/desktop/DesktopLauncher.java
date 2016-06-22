6a29608f767e5a5d6d9264df065af3b2336666e2
Costyuk <CCostyuk@kiev.luxoft.com>
2016-06-22 18:56:42 +0300

    Initial implementaion based on libgdx engine

---
+++ desktop/src/com/costyuk/leprechaun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.costyuk.leprechaun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.costyuk.leprechaun.HappyLeprechaun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HappyLeprechaun(), config);
+	}
+}
